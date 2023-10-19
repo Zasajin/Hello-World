c4fc8c598658c899628a63b02fe448037ce1ae2a
Vince <vincent.hawel@gmail.com>
2023-10-19 19:44:14 +0200

    Assignment finished

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+function getRandomColor() {
+    
+    var randomNumber = Math.floor(Math.random() * 5) + 1;
+    var colorElement = document.getElementById('colorElement');
+    colorElement.className = 'color' + randomNumber;
+  }
 
+  var colorButton = document.getElementById('colorButton');
+  colorButton.addEventListener('click', getRandomColor);
+  
